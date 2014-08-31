6977fea58123fcb698107132138df22c029e9315
dhacker29 <dhackerdvm@gmail.com>
2014-08-31 16:26:45 -0400

    find7: Update blobs
    Change-Id: I334044a021b3d98a3a9789e5d73a8199cb864b85

--- find7/proprietary/app/Android.mk
+++ find7/proprietary/app/Android.mk
@@ -19,15 +19,6 @@ LOCAL_PATH := $(call my-dir)
 ifneq ($(filter find7 find7u,$(TARGET_DEVICE)),)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := com.qualcomm.location
-LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := com.qualcomm.location.apk
-LOCAL_CERTIFICATE := platform
-LOCAL_MODULE_CLASS := APPS
-LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := PPPreference
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := PPPreference.apk
